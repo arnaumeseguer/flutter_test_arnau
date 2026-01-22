c57b6aae685975b445753b2acb46d88e5ebfd17f
arnaumeseguer <arnaumeseguer@iesebre.com>
2026-01-22 17:13:54 +0100

    Refactoritzacio dels providers

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,55 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:first_flutter/data/models/sentence.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:provider/provider.dart';
 import 'package:first_flutter/main.dart';
+import 'package:first_flutter/data/repositories/sentence_repository.dart';
+import 'package:first_flutter/data/services/sentence_service.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  Widget crearMultiProvider() {
+    return MultiProvider(
+      providers: [
+        Provider<ISentenceService>(create: (_) => FakeSentenceService()),
+        Provider<ISentenceRepository>(
+          create: (context) =>
+              SentenceRepository(sentenceService: context.read()),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test clicar botó Next', (WidgetTester tester) async {
+    await tester.pumpWidget(crearMultiProvider());
+    await tester.pumpAndSettle();
+    // Busquem el botó Next i el cliquem
+    final nextButton = find.widgetWithText(ElevatedButton, 'Next');
+    expect(nextButton, findsOneWidget);
+    await tester.tap(nextButton);
+    await tester.pumpAndSettle();
+    //La frase encara hi és (perquè el mock sempre retorna el mateix)
+    expect(find.text('Test sentence'), findsNWidgets(2));
   });
+  testWidgets('Test mostra frase inicial', (WidgetTester tester) async {
+    await tester.pumpWidget(crearMultiProvider());
+    await tester.pumpAndSettle();
+    expect(find.text('Test sentence'), findsOneWidget);
+  });
+}
+
+class FakeSentenceService implements ISentenceService {
+  @override
+  Future<Sentence> getNext() async {
+    // Retornem una frase fixa, sense HTTP!
+    return Sentence(text: 'Test sentence');
+  }
+
+  @override
+  Future<Sentence> createSentence(String text) async {
+    return Sentence(text: text);
+  }
 }
